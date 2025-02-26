27f914da6ee2e0073c34373e3474299d75ba4722
harkonennen <harri.antti.harkonen@gmail.com>
2025-02-26 16:59:54 +0200

    Tehtava 21

---
+++ viikko7/Tehtava21/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var opiskelijaRouter = require('./routes/opiskelija');
+var opintojaksoRouter = require('./routes/opintojakso');
+var arviointiRouter = require('./routes/arviointi');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/opiskelija', opiskelijaRouter);
+app.use('/arviointi', arviointiRouter);
+app.use('/opintojakso', opintojaksoRouter);
+
+module.exports = app;
